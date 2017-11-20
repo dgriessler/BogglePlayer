1defc7d53d56967ecb6fbd6658ac00b094b0b4b9
Dan Levy <danlevy124@gmail.com>
2017-11-20 13:20:08 -0500

    Add files via upload

---
+++ BogglePlayer.java
@@ -0,0 +1,25 @@
+/*
+  Authors (group members): Dan Levy, Daniel Griessler, and Frank Savino
+  Email addresses of group members: dlevy2016@my.fit.edu, dgriessler2016@my.fit.edu, and fsavino2018@my.fit.edu
+  Group name: Group 2b
+  Course: CSE 2010
+  Section: 02
+  Description of the overall algorithm and key data structures:
+*/
+
+public class BogglePlayer {
+	/*
+	 * 
+	 */
+    public BogglePlayer(String wordFile) {
+
+    }
+
+    /*
+     * 
+     */
+    public Word[] getWords(char[][] board) {
+    		Word[] myWords = new Word[20];
+    		return myWords;
+    }
+}
